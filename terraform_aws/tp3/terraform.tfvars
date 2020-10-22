6029b58e4bdc9a2ac2faa233cefa3232b219ca3d
barry <barry2abdulrahim@gmail.com>
2020-10-22 19:43:21 +0000

    aws provider

---
+++ terraform_aws/tp3/terraform.tfvars
@@ -0,0 +1,10 @@
+myinstance = "t2.nano"
+
+my_secret_key = "YOU SECRET KEY"
+my_acces_key = "YOU ACCES KEY"
+
+my_region = "us-east-1"
+
+mytag = {
+  Name = "mytag_2"
+}
---
+++ terraform_aws/tp3/variables.tf
@@ -0,0 +1,20 @@
+variable "myinstance" {
+  type    = "string"
+  default = "t2.micro"
+}
+
+variable "mytag" {
+  type    = "map"
+}
+
+variable "my_secret_key" {
+  type    = "string"
+}
+
+variable "my_acces_key" {
+  type    = "string"
+}
+
+variable "my_region" {
+  type    = "string"
+}
