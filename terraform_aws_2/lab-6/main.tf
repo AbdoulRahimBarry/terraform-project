9a1764aa2159ef2a127b37467f458f1bffee3773
barry <50138085+AbdoulRahimBarry@users.noreply.github.com>
2020-10-26 22:22:01 +0100

    Update main.tf

--- terraform_aws_2/lab-6/main.tf
+++ terraform_aws_2/lab-6/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAW7UYPY6KTF5JSLET"
-  secret_key = "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"
+  access_key = "YOU ACCES KEY"
+  secret_key = "YOU SECRET KEY"
 }
 
 resource "aws_iam_user" "lb" {
